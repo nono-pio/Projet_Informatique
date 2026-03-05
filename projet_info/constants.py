c4360b4f4af30e5a78e6dda953a17427171c0a67
Nolan Piccand <nolan.piccand@gmail.com>
2026-03-05 09:59:57 +0100

    projet uv + arcade

---
+++ projet_info/constants.py
@@ -0,0 +1,11 @@
+WINDOW_TITLE = "Adventure"
+"""Title of the main window."""
+
+SCALE = 2.0
+"""The global scale for all textures."""
+
+TILE_SIZE = 32
+"""After scaling, the size of a tile."""
+
+MAX_WINDOW_WIDTH = 14 * TILE_SIZE
+MAX_WINDOW_HEIGHT = 14 * TILE_SIZE
